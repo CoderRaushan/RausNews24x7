8ef32293c481fad7277293aee0feefcf1402d636
CoderRaushan <raushankumar23082004@gmail.com>
2024-09-07 08:39:21 +0530

    robots.txt file  added

--- app.js
+++ app.js
@@ -112,7 +112,7 @@ app.use((req, res, next) => {
 
 app.use("/news", newsRouter);
 app.use("/news/:id/reviews",reviewRouter);
-app.use("/",newsRouter);
+// app.use("/",newsRouter);
 app.use("/",userRouter);
 
  
